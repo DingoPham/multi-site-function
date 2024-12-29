860f558e90d6e7459cfefb952ec26d93298a830b
DingoPham <pencibambo@gmail.com>
2024-12-29 23:33:02 +0700

    test 2

--- components/component-webpages/Footer.vue
+++ components/component-webpages/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="w-100per h-custom bg-c c-white">
-    <p>FunGame</p>
+    <div class="t-center flex flex-direction justify-content h-100per">
+      <h1 class="">FunGame</h1>
+      <p>
+        @DingoPham
+      </p>
+      <p>
+        Bring enjoy time to people by <a href="https://dingopham.github.io/">DingoPham</a>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -20,5 +28,18 @@
   }
   p{
     margin: 0;
+    font-size: 20px;
+  }
+  h1{
+    margin:  0;
+    font-size: 35px;
+  }
+  a{
+    color: #fff;
+    font-weight: 600;
+    text-decoration: none;
+  }
+  a:hover{
+    text-decoration: underline;
   }
 </style>
